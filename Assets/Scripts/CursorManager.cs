d966e79c8afc9773ce9c93bfc643138ab9d7ea62
Ankur Jain <ankur.jain@nsitonline.in>
2016-05-02 17:48:21 -0700

    Added directional indicator.. Updated gaze manager

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,53 @@
+﻿using HoloToolkit;
+using UnityEngine;
+
+/// <summary>
+/// CursorManager class takes Cursor GameObjects.
+/// One that is on Holograms and another off Holograms.
+/// Shows the appropriate Cursor when a Hologram is hit.
+/// Places the appropriate Cursor at the hit position.
+/// Matches the Cursor normal to the hit surface.
+/// </summary>
+public class CursorManager : Singleton<CursorManager>
+{
+    [Tooltip("Drag the Cursor object to show when it hits a hologram.")]
+    public GameObject CursorOnHolograms;
+
+    [Tooltip("Drag the Cursor object to show when it does not hit a hologram.")]
+    public GameObject CursorOffHolograms;
+
+    void Awake()
+    {
+        if (CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+
+        // Hide the Cursors to begin with.
+        CursorOnHolograms.SetActive(false);
+        CursorOffHolograms.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (GazeManager.Instance == null || CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+
+        if (GazeManager.Instance.Hit)
+        {
+            CursorOnHolograms.SetActive(true);
+            CursorOffHolograms.SetActive(false);
+        }
+        else
+        {
+            CursorOffHolograms.SetActive(true);
+            CursorOnHolograms.SetActive(false);
+        }
+
+        gameObject.transform.position = GazeManager.Instance.Position;
+
+        gameObject.transform.up = GazeManager.Instance.Normal;
+    }
+}
---
+++ Assets/Scripts/InteractibleManager.cs
@@ -0,0 +1,67 @@
+﻿using HoloToolkit;
+using UnityEngine;
+
+/// <summary>
+/// InteractibleManager keeps tracks of which GameObject
+/// is currently in focus.
+/// </summary>
+public class InteractibleManager : Singleton<InteractibleManager>
+{
+    public GameObject FocusedGameObject { get; private set; }
+
+    private GameObject oldFocusedGameObject = null;
+
+    void Start()
+    {
+        FocusedGameObject = null;
+    }
+
+    void Update()
+    {
+        oldFocusedGameObject = FocusedGameObject;
+
+        if (GazeManager.Instance.Hit)
+        {
+            RaycastHit hitInfo = GazeManager.Instance.HitInfo;
+            if (hitInfo.collider != null)
+            {
+                // Assign the hitInfo's collider gameObject to the FocusedGameObject.
+                FocusedGameObject = hitInfo.collider.gameObject;
+            }
+            else
+            {
+                FocusedGameObject = null;
+            }
+        }
+        else
+        {
+            FocusedGameObject = null;
+        }
+
+        if (FocusedGameObject != oldFocusedGameObject)
+        {
+            ResetFocusedInteractible();
+
+            if (FocusedGameObject != null)
+            {
+                if (FocusedGameObject.GetComponent<Interactible>() != null)
+                {
+                    // Send a GazeEntered message to the FocusedGameObject.
+                    FocusedGameObject.SendMessage("GazeEntered");
+                }
+            }
+        }
+    }
+
+    private void ResetFocusedInteractible()
+    {
+        if (oldFocusedGameObject != null)
+        {
+            if (oldFocusedGameObject.GetComponent<Interactible>() != null)
+            {
+                // Send a GazeExited message to the oldFocusedGameObject.
+                oldFocusedGameObject.SendMessage("GazeExited");
+            }
+        }
+    }
+}
